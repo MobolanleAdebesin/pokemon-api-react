13baff88ade1ee07a8599ade787916cdea91203a
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-12-31 09:57:15 -0500

    Reformatted conditional rendering so that when the button is clicked information about the pokemon's stats are displayed. Added comments to code

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ class App extends Component {
       data: []
     };
   }
+  //Retrieve data for the first 150 pokemon from the poke API
   componentDidMount() {
     axios
       .get(url)
       .then(function(res) {
-        // console.log(res.data.results);
         let results = res.data.results;
         return results;
       })
@@ -37,12 +37,15 @@ class App extends Component {
     return (
       <div className="App">
         <Router>
+          {/* The navigation bar is being rendered outside of the routes so that it appears on every route/page */}
           <nav>
             <Link to="/">Pokemon</Link>
             <Link to="/Home">Home</Link>
             <Link to="/About">About</Link>
           </nav>
+          {/* Defined routes */}
           <Switch>
+            {/* This route leads to a 'detail' page about each pokemon. When the link is clicked the pokemon's id number is used to pull data from the pokemon API and a pokemon image API */}
             <Route
               path="/Pokemon/:id"
               render={routerProps => (
@@ -51,6 +54,7 @@ class App extends Component {
             />
             <Route path="/Home" component={Home} />
             <Route path="/About" component={About} />
+            {/* This route leads to the pokemon list. The information about all 150 information is passed  from the App state to the PokemonList component as routerProps */}
             <Route
               path="/"
               render={routerProps => (
--- src/PokemonPage.js
+++ src/PokemonPage.js
@@ -6,28 +6,89 @@ class PokemonPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: 0
+      id: "",
+      pokemonData: {}
     };
   }
+  //When the page loads, save the params.id into state so that number can be accessed throughout the component.
   componentDidMount() {
     const { params } = this.props.match;
     this.setState({ id: params.id });
   }
-
+  // When the user clicks the button element, this function is called. It fetches data from the pokemon API at the endpoint specific to the pokemon in this detail page
   getData = () => {
-    axios.get("");
+    let pokeIndex = parseInt(this.state.id) + 1;
+    let url = `https://pokeapi.co/api/v2/pokemon/${pokeIndex}`;
+    // console.log(url);
+    axios
+      .get(url)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ pokemonData: res.data });
+        //Abilities
+        // First four moves
+        // Types
+        // Stats:  Speed Attack Defense HP
+      })
+      .catch(err => {
+        console.log("There was an error " + err);
+      });
   };
   render() {
+    // Uses the id saved in state to identify the selected pokemon in the props array (which contains all 150 pokemon)
+    //That pokemon's information is then used to populate the page.
     if (this.props.data[this.state.id]) {
       let pokemon = this.props.data[this.state.id];
-      console.log(pokemon.name);
+      let imageIndex = parseInt(this.state.id) + 1;
+      if (this.state.pokemonData.types) {
+        return (
+          <div>
+            <h3>{pokemon.name}</h3>
+            <img
+              className="PokemonPage-image"
+              src={`https://pokeres.bastionbot.org/images/pokemon/${imageIndex}.png`}
+              alt={pokemon.name}
+            />
+            <button onClick={this.getData}>{pokemon.name}</button>
+            <div>
+              <h3>Stats</h3>
+              <h5>Type</h5>
+              <ul>
+                {this.state.pokemonData.types.map((type, i) => {
+                  return <li key={i}>{type.type.name}</li>;
+                })}
+              </ul>
+              <h5>Abilities</h5>
+              <ul>
+                {this.state.pokemonData.abilities.map((ability, i) => {
+                  return <li key={i}>{ability.ability.name}</li>;
+                })}
+              </ul>
+              <h5>Stats</h5>
+              <ul>
+                {this.state.pokemonData.stats.map((stat, i) => {
+                  return (
+                    <li key={i}>{`${stat.stat.name}: ${stat.base_stat}`}</li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        );
+      }
       return (
         <div>
           <h3>{pokemon.name}</h3>
+          <img
+            className="PokemonPage-image"
+            src={`https://pokeres.bastionbot.org/images/pokemon/${imageIndex}.png`}
+            alt={pokemon.name}
+          />
+          <button onClick={this.getData}>{pokemon.name}</button>
         </div>
       );
     }
-
+    //If the state has not updated yet then this "...Loading" element will be rendered to avoid an error.
     return (
       <div>
         <h1>Poke!</h1>
